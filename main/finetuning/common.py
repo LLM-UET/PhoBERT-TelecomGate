2b9df02c23a1b261a983daf6206cf57e86d5f31b
Vu Tung Lam <vutunglaminfo@gmail.com>
2025-11-15 19:38:20 +0700

    feat: allow NUM_EPOCHS to be set via environment variable

--- main/finetuning/common.py
+++ main/finetuning/common.py
@@ -1,8 +1,10 @@
+import os
+
 # Maximum length of an input sequence after tokenization
 MAX_LEN = 128
 
 BATCH_SIZE = 8 # for 16GB GPU, fp32
 
-NUM_EPOCHS = 3
+NUM_EPOCHS = int(os.getenv("NUM_EPOCHS", 3))
 
 LEARNING_RATE = 2e-5
