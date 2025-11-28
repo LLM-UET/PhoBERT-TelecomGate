d3edc94a5454ae24403177057193e8f214cdaf9d
Vu Tung Lam <vutunglaminfo@gmail.com>
2025-11-28 23:13:43 +0700

    fix: prevent datasets.load_dataset from inferring types automatically

--- main/data/loader.py
+++ main/data/loader.py
@@ -1,19 +1,33 @@
-from datasets import load_dataset
+from datasets import load_dataset, Features, Value
 
 def load_csv_dataset(file_path, column_names=["text", "label"]):
+    # Force every column to be string
+    features = Features({
+        col: Value("string") for col in column_names
+    })
+
     dataset = load_dataset(
         "csv",
         data_files=file_path,
         column_names=column_names,
-    )["train"] # single file -> only "train" split
+        features=features,  # <-- prevents dtype inference
+    )
+    
+    dataset = dataset["train"] # type: ignore
 
     return dataset
 
 def load_many_csv_datasets(file_paths, column_names=["text", "label"]):
+    # Force every column to be string
+    features = Features({
+        col: Value("string") for col in column_names
+    })
+
     dataset = load_dataset(
         "csv",
         data_files=file_paths,
         column_names=column_names,
+        features=features,
     )
 
     return dataset
